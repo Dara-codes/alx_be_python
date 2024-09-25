859b42f82db6badfd2fb1b5f30a577222c78bc07
Dara-codes <josephdaramola607@gmail.com>
2024-09-25 11:54:56 +0100

    match case exp

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,24 @@
+num1 = int(input("Enter the first number:").strip())
+num2 = int(input("Enter the second number:").strip())
+
+operations = input("Choose the operation (+, -, *, /):")
+
+match operations:
+    case "+":
+        result = num1 + num2
+        print(f"The result of {num1} + {num2} is {result}")
+    case "-":
+        result = num1 - num2
+        print(f"The result of {num1} - {num2} is {result}")
+    case "*":
+        result = num1 * num2
+        print(f"The result of {num1} * {num2} is {result}")
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+            print(f"The result of {num1} / {num2} is {result}")
+        else:
+            print(f"Division by zero is not allowed here!")
+    case _:
+        print("invalid!!!, choose within the given operations(+,-,*,/)")
+        
