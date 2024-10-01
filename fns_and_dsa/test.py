0933d3e174c21711392659c35e588edd0d3f48e6
Dara-codes <josephdaramola607@gmail.com>
2024-10-01 16:34:40 +0100

    temp_conversion

---
+++ fns_and_dsa/test.py
@@ -0,0 +1,10 @@
+def addition(x, y):
+
+    x = float(input("the value of X "))
+    y = float(input("the value of y "))
+
+    add = x + y
+    return add
+result = addition(1,2)
+
+print ("addition is ", result)
