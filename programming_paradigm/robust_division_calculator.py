49f247cf0231799db69b5e42e9f464e5acae147b
Dara-codes <josephdaramola607@gmail.com>
2024-10-07 05:06:19 +0100

    understanding oop, robust_division cal

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,9 +4,8 @@ def safe_divide(numerator, denominator):
          den = float(denominator)
 
          division= num/den
-         print(f"The result of the division is {division}")
-
+         return(f"The result of the division is {division}")
     except ZeroDivisionError:
-         print("Error: Cannot divide by zero.")
+         return("Error: Cannot divide by zero")
     except ValueError:
-         print("Error: Please enter numeric values only.")
+         return("Error: Please enter numeric values only")
