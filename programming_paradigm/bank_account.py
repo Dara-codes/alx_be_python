cfe70fe50272b186e46870e443d5ac7a1e7e55ff
Dara-codes <josephdaramola607@gmail.com>
2024-10-07 04:05:26 +0100

    understanding oop, bank_account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+class BankAccount:
+    def __init__(self, initial_balance = 0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+            if amount > 0:
+                self.account_balance += amount
+                print(f"new balance {self.account_balance}")
+            else:
+                print(" doesnt accept negative digits")
+        
+    def withdraw(self, amount):
+            if amount > 0:
+                if amount <= self.__account_balance:
+                    self.account_balance -= amount
+                    print(f"successful, {self.account_balance}")
+                    return True
+                else:
+                    print("Insufficient funds")
+                    return False
+            else:
+                print("no negative input")
+                return False
+
+    def display_balance(self):
+            print(f"Current Balance: $ {self.account_balance}") 
